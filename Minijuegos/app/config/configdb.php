1b03893931d973e7d34154dc79f41461e3c05487
mlopezEVG <mlopezvega.guadalupe@alumnado.fundacionloyola.net>
2025-02-21 10:33:16 +0100

    Delete Minijuegos directory

--- Minijuegos/app/config/configdb.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Datos de conexión de la bbdd minijuegos.
-DEFINE('HOST', 'localhost');
-DEFINE('USER', 'root');
-DEFINE('PW','');
-DEFINE('BBDD','minijuegos');
-DEFINE('CHARSET', 'utf8');  //Establecemos el juego de caracteres de la conexón de la bbdd.
-
-?>
